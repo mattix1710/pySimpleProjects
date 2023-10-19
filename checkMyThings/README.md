dea2cfab01776d0b3ff62f9950c3fbccea8b6e9e
Mateusz Szuda <mateusz.szuda@wp.pl>
2023-10-19 21:28:19 +0200

    [checkMyThings]-init

---
+++ checkMyThings/README.md
@@ -0,0 +1,7 @@
+# checkMyThings
+
+IDEA: simple app for checklists and similar stuff
+
+---
+
+!! Project abandoned for now, sorry :/
---
+++ checkMyThings/main.py
@@ -0,0 +1,18 @@
+# standard number of imports
+
+from PyQt6.QtWidgets import QApplication
+import sys
+
+import windowInit
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    app.setStyleSheet("""QPushButton{
+                            color: blue;
+                            font-size: 25px;
+                            font-weight: 500;
+                    }""")
+
+    window = windowInit.MainWindow()
+    window.show()
+    sys.exit(app.exec())
